c9317a7a31f719e6745b6e9dff4f09c199de3ea9
Sam Xu <saxu@microsoft.com>
2016-05-20 09:51:47 +0800

    1. Add an emtpy solution, It can used to add more test project into. 2. Add IsOf test cases.

---
+++ Microsoft.AspNet.OData.Test/Isof/IsOfTest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using Microsoft.OData.Edm;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.AspNet.OData.Test.Isof
+{
+    public class IsOfTest
+    {
+        [Fact]
+        public void Isof_SubType_Metadata()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<A>("As");
+            IEdmModel model = builder.GetEdmModel();
+
+            var configuration = new[] { typeof(MetadataController) }.GetHttpConfiguration();
+            configuration.MapODataServiceRoute("odata", "odata", model);
+            HttpServer server = new HttpServer(configuration);
+            var client = new HttpClient(server);
+
+            var response = client.GetAsync("http://localhost/odata/$metadata").Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result); 
+        }
+
+        [Fact]
+        public void Isof_SubType_Test()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<A>("As");
+            IEdmModel model = builder.GetEdmModel();
+
+            var configuration = new[] { typeof(AsController) }.GetHttpConfiguration();
+            configuration.MapODataServiceRoute("odata", "odata", model);
+            HttpServer server = new HttpServer(configuration);
+            var client = new HttpClient(server);
+
+            // Query As only the Dp is a "E" type.
+            string payload = @"{
+  ""@odata.context"":""http://localhost/odata/$metadata#As"",""value"":[
+    {
+      ""@odata.type"":""#Microsoft.AspNet.OData.Test.Isof.B"",""Id"":1,""Dp"":{
+        ""@odata.type"":""#Microsoft.AspNet.OData.Test.Isof.E"",""Prop"":""E Prop"",""Ep"":""E kind""
+      },""Bp"":7
+    }
+  ]
+}";
+            var response = client.GetAsync("http://localhost/odata/As?$filter=isof(Dp,'Microsoft.AspNet.OData.Test.Isof.E')").Result;
+            Assert.Equal(payload, response.Content.ReadAsStringAsync().Result);
+
+            // Query As only the Dp is a "F" type.
+            payload = @"{
+  ""@odata.context"":""http://localhost/odata/$metadata#As"",""value"":[
+    {
+      ""@odata.type"":""#Microsoft.AspNet.OData.Test.Isof.B"",""Id"":2,""Dp"":{
+        ""@odata.type"":""#Microsoft.AspNet.OData.Test.Isof.F"",""Prop"":""FFFF Prop"",""Fp"":""FFFF kind""
+      },""Bp"":8
+    }
+  ]
+}";
+            response = client.GetAsync("http://localhost/odata/As?$filter=isof(Dp,'Microsoft.AspNet.OData.Test.Isof.F')").Result;
+            Assert.Equal(payload, response.Content.ReadAsStringAsync().Result);
+        }
+
+        public class AsController : ODataController
+        {
+            private static IList<A> _as;
+
+            static AsController()
+            {
+                _as = new List<A>();
+
+                A a = new B
+                {
+                    Id = 1,
+                    Bp = 7,
+                    Dp = new E
+                    {
+                        Ep = "E kind",
+                        Prop = "E Prop"
+                    }
+                };
+                _as.Add(a);
+
+                a = new B
+                {
+                    Id = 2,
+                    Bp = 8,
+                    Dp = new F
+                    {
+                        Fp = "FFFF kind",
+                        Prop = "FFFF Prop"
+                    }
+                };
+                _as.Add(a);
+            }
+
+            [EnableQuery]
+            public IHttpActionResult Get()
+            {
+                return Ok(_as);
+            }
+        }
+
+        public abstract class A
+        {
+            public int Id { get; set; }
+            public D Dp { get; set; }
+        }
+
+        public class B : A
+        {
+            public int Bp { get; set; }
+        }
+
+        public abstract class D
+        {
+            public string Prop { get; set; }
+        }
+
+        public class E : D
+        {
+            public string Ep { get; set; }
+        }
+
+        public class F : D
+        {
+            public string Fp { get; set; }
+        }
+    }
+}
---
+++ Microsoft.AspNet.OData.Test/TypeExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace Microsoft.AspNet.OData.Test
+{
+    internal static class TypeExtensions
+    {
+        public static HttpConfiguration GetHttpConfiguration(this Type[] controllers)
+        {
+            var resolver = new TestAssemblyResolver(new MockAssembly(controllers));
+            var configuration = new HttpConfiguration();
+            configuration.Services.Replace(typeof(IAssembliesResolver), resolver);
+            return configuration;
+        }
+    }
+
+    internal sealed class MockAssembly : Assembly
+    {
+        Type[] _types;
+
+        public MockAssembly(params Type[] types)
+        {
+            _types = types;
+        }
+
+        public override Type[] GetTypes()
+        {
+            return _types;
+        }
+    }
+
+    internal class TestAssemblyResolver : IAssembliesResolver
+    {
+        List<Assembly> _assemblies;
+
+        public TestAssemblyResolver(MockAssembly assembly)
+        {
+            _assemblies = new List<Assembly>();
+            _assemblies.Add(assembly);
+        }
+
+        public TestAssemblyResolver(params Type[] types)
+            : this(new MockAssembly(types))
+        {
+        }
+
+        public ICollection<Assembly> GetAssemblies()
+        {
+            return _assemblies;
+        }
+    }
+}
