5b9c203e4c6836bddb23883120d2c729ff49de7e
Sam Xu <saxu@microsoft.com>
2016-05-29 23:44:57 -0700

    add ETag tests, uring my own etag handler

---
+++ Microsoft.AspNet.OData.Test/ETag/ETagTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using System.Web.OData.Formatter;
+using Microsoft.OData.Edm;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.AspNet.OData.Test.ETag
+{
+    public class ETagTest
+    {
+        [Fact]
+        public void CanQueryEntityWithDefaultETagHandler()
+        {
+            IEdmModel model = GetEdmModel();
+            HttpConfiguration configuration = new[] { typeof(CustomersController) }.GetHttpConfiguration();
+            configuration.MapODataServiceRoute("odata", "odata", model);
+            HttpServer server = new HttpServer(configuration);
+            HttpClient client = new HttpClient(server);
+
+            string requestUri = "http://localhost/odata/Customers(1)";
+
+            HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("Get"), requestUri);
+            HttpResponseMessage response = client.SendAsync(request).Result;
+
+            response.EnsureSuccessStatusCode();
+
+            JObject result = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            Console.WriteLine(result);
+            Assert.Equal("W/\"dHJ1ZQ==,OTk=\"", result["@odata.etag"]);
+        }
+
+        [Fact]
+        public void CanQueryEntityWithCustomETagHandler()
+        {
+            IEdmModel model = GetEdmModel();
+
+            IETagHandler handler = new MyETagHandler(); // MyETag
+            HttpConfiguration configuration = new[] { typeof(CustomersController) }.GetHttpConfiguration();
+            configuration.SetETagHandler(handler);
+            configuration.MapODataServiceRoute("odata", "odata", model);
+            HttpServer server = new HttpServer(configuration);
+            HttpClient client = new HttpClient(server);
+
+            string requestUri = "http://localhost/odata/Customers(1)";
+
+            HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("Get"), requestUri);
+            HttpResponseMessage response = client.SendAsync(request).Result;
+
+            response.EnsureSuccessStatusCode();
+
+            JObject result = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            Console.WriteLine(result);
+            Assert.Equal("W/\"IsOk,true/Salary,99\"", result["@odata.etag"]);
+        }
+
+        [Fact]
+        public void CustomETagHandlerRoundTrip()
+        {
+            Dictionary<string, object> properties = new Dictionary<string, object>
+            {
+                { "BooleanProperty", (bool)true },
+                { "IntProperty", (int)123},
+                { "GuidProperty", Guid.Empty}
+            };
+
+            IETagHandler handler = new MyETagHandler(); // MyETag
+            EntityTagHeaderValue etagHeaderValue = handler.CreateETag(properties);
+            IDictionary<string, object> values = handler.ParseETag(etagHeaderValue);
+
+            Assert.Equal(properties, values);
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Customer>("Customers");
+            return builder.GetEdmModel();
+        }
+
+        public class CustomersController : ODataController
+        {
+            [EnableQuery]
+            public IHttpActionResult Get(int key)
+            {
+                return Ok(_customers.Single(c => c.Id == key));
+            }
+
+            private static IList<Customer> _customers;
+
+            static CustomersController()
+            {
+                _customers = new List<Customer>
+                {
+                    new Customer { Id = 1, Salary = 99, IsOk = true},
+                    new Customer { Id = 2, Salary = 12, IsOk = false}
+                };
+            }
+        }
+
+        public class Customer
+        {
+            public int Id { get; set; }
+
+            [ConcurrencyCheck]
+            public int Salary { get; set; }
+
+            [ConcurrencyCheck]
+            public bool IsOk { get; set; }
+        }
+    }
+}
---
+++ Microsoft.AspNet.OData.Test/ETag/MyETagHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.OData.Formatter;
+using Microsoft.OData.Core;
+using Microsoft.OData.Core.UriParser;
+
+namespace Microsoft.AspNet.OData.Test.ETag
+{
+    // It's just for test.
+    public class MyETagHandler : IETagHandler
+    {
+        public EntityTagHeaderValue CreateETag(IDictionary<string, object> properties)
+        {
+            Debug.Assert(properties != null);
+
+            if (properties.Count == 0)
+            {
+                return null;
+            }
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append('\"');// necessary
+            bool firstProperty = true;
+            foreach (var property in properties)
+            {
+                if (firstProperty)
+                {
+                    firstProperty = false;
+                }
+                else
+                {
+                    builder.Append("/");
+                }
+
+                builder.Append(property.Key).Append(",");
+                string value = ODataUriUtils.ConvertToUriLiteral(property.Value, ODataVersion.V4);
+                builder.Append(value);
+            }
+
+            builder.Append('\"'); // necessary
+            string tag = builder.ToString();
+            return new EntityTagHeaderValue(tag, isWeak: true);
+        }
+
+        public IDictionary<string, object> ParseETag(EntityTagHeaderValue etagHeaderValue)
+        {
+            string[] tags = etagHeaderValue.Tag.Trim('\"').Split('/');
+
+            IDictionary<string, object> properties = new Dictionary<string, object>();
+            for (int index = 0; index < tags.Length; index++)
+            {
+                string[] rawValues = tags[index].Split(',');
+
+                object obj = ODataUriUtils.ConvertFromUriLiteral(rawValues[1], ODataVersion.V4);
+                properties.Add(rawValues[0], obj);
+            }
+
+            return properties;
+        }
+    }
+}
