4530c655a0874f869c371a4bad75e7d2f5c47b71
Sam Xu <saxu@microsoft.com>
2016-06-06 20:17:03 -0700

    add one test case about the function paraemter resolve

---
+++ Microsoft.AspNet.OData.Test/Function/CustomFunctionResolve/CustomControllerRoutingConvention.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using System.Web.OData.Routing;
+using System.Web.OData.Routing.Conventions;
+using Microsoft.OData.Core;
+using Microsoft.OData.Edm;
+
+namespace Microsoft.AspNet.OData.Test.Function.CustomFunctionResolve
+{
+    public class CustomControllerRoutingConvention : NavigationSourceRoutingConvention
+    {
+        public override string SelectAction(ODataPath odataPath, HttpControllerContext context,
+            ILookup<string, HttpActionDescriptor> actionMap)
+        {
+            string action = null;
+
+            if (odataPath.PathTemplate == "~/entityset/function")
+            {
+                var segment = odataPath.Segments[odataPath.Segments.Count - 1] as BoundFunctionPathSegment;
+                action = segment.Function.Name;
+                AddFunctionParametersToRouteData(context, segment);
+            }
+
+            return action;
+        }
+
+        private static void AddFunctionParametersToRouteData(HttpControllerContext controllerContext,
+            BoundFunctionPathSegment functionSegment)
+        {
+            // skip the first binding parameter
+            for (int i = 1; i < functionSegment.Function.Parameters.Count(); i++)
+            {
+                var param = functionSegment.Function.Parameters.ElementAt(i);
+                string name = param.Name;
+
+                object value = functionSegment.GetParameterValue(name);
+                ODataEnumValue enumValue = value as ODataEnumValue;
+                if (enumValue != null)
+                {
+                    // Remove the type name of the ODataEnumValue and keep the value.
+                    value = enumValue.Value;
+                }
+
+                controllerContext.RouteData.Values.Add(name, value);
+
+                Type type = typeof(ODataConventionModelBuilder).Assembly.GetTypes().First(c => c.Name == "ODataParameterValue");
+                object instance = Activator.CreateInstance(type, value, param.Type);
+                controllerContext.Request.ODataProperties().RoutingConventionsStore.Add("DF908045-6922-46A0-82F2-2F6E7F43D1B1_" + name, instance);
+            }
+        }
+    }
+}
---
+++ Microsoft.AspNet.OData.Test/Function/CustomFunctionResolve/CustomFunctionParameterResolveTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+using System.Web.OData.Query;
+using System.Web.OData.Routing;
+using Microsoft.AspNet.OData.Test.Function.CustomFunctionResolve;
+using Microsoft.OData.Edm;
+using Xunit;
+
+namespace Microsoft.AspNet.OData.Test.Function
+{
+    public class CustomFunctionParameterResolveTest
+    {
+        [Fact]
+        public void FunctionParameterResolveUsingCustomerRoutingConvention()
+        {
+            var configuration = new[] { typeof(MetadataController), typeof(CustomersController) }.GetHttpConfiguration();
+
+            var conventions = System.Web.OData.Routing.Conventions.ODataRoutingConventions.CreateDefault();
+
+            conventions.Insert(0, new CustomControllerRoutingConvention());
+
+            configuration.MapODataServiceRoute("odata", "odata", GetEdmModel(), new DefaultODataPathHandler(), conventions);
+            HttpServer server = new HttpServer(configuration);
+            var client = new HttpClient(server);
+
+            var response = client.GetAsync("http://localhost/odata/Customers/Default.TestFunc(coll=[1,2,3],param='test')").Result;
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Customer>("Customers");
+
+            var func = builder.EntityType<Customer>().Collection
+                                       .Function("TestFunc")
+                                       .Returns<IEnumerable<string>>();
+            func.CollectionParameter<int>("coll");
+            func.Parameter<string>("param");
+
+            return builder.GetEdmModel();
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class CustomersController : ODataController
+    {
+        [HttpGet]
+        public IHttpActionResult TestFunc(
+            // [FromODataUri] long customerId,
+            // [FromODataUri] long accountId,
+            [FromODataUri] IEnumerable<int> coll,
+            [FromODataUri] string param,
+            ODataQueryOptions<Customer> options)
+        {
+            Assert.True(coll != null);
+            Assert.Equal(new [] { 1, 2, 3 }, coll);
+            Assert.Equal("test", param);
+
+            return Ok();
+        }
+    }
+}
