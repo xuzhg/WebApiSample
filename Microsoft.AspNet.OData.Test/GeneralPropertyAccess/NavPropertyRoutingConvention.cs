671ed7f2de4d1a20874fa2cc8688b9a2ff4a755c
Sam Xu <saxu@microsoft.com>
2018-01-09 16:22:47 -0800

    add the navigation property entity access

---
+++ Microsoft.AspNet.OData.Test/GeneralPropertyAccess/NavPropertyRoutingConvention.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.OData.Routing;
+using System.Web.OData.Routing.Conventions;
+using Microsoft.OData.Core;
+using Microsoft.OData.Core.UriParser;
+using Microsoft.OData.Edm;
+
+namespace Microsoft.AspNet.OData.Test.GeneralPropertyAccess
+{
+    public class NavPropertyRoutingConvention : NavigationRoutingConvention
+    {
+        public override string SelectAction(ODataPath odataPath, HttpControllerContext controllerContext, ILookup<string, HttpActionDescriptor> actionMap)
+        {
+            HttpMethod httpMethod = controllerContext.Request.Method;
+            if (httpMethod != HttpMethod.Get)
+            {
+                return null;
+            }
+
+            if (odataPath.PathTemplate == "~/entityset/key/navigation/key")
+            {
+                KeyValuePathSegment segment = (KeyValuePathSegment)odataPath.Segments[1];
+                object value = ODataUriUtils.ConvertFromUriLiteral(segment.Value, ODataVersion.V4);
+                controllerContext.RouteData.Values.Add("key", value);
+
+                segment = (KeyValuePathSegment)odataPath.Segments[3];
+                value = ODataUriUtils.ConvertFromUriLiteral(segment.Value, ODataVersion.V4);
+                controllerContext.RouteData.Values.Add("relatedKey", value);
+
+                NavigationPathSegment property = odataPath.Segments[2] as NavigationPathSegment;
+                // controllerContext.RouteData.Values.Add("propertyName", property.NavigationProperty.Name);
+
+                return "Get" + property.NavigationProperty.Name;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Microsoft.AspNet.OData.Test/GeneralPropertyAccess/NavigationPropertyAccessTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+using System.Web.OData.Extensions;
+using System.Web.OData.Routing;
+using System.Web.OData.Routing.Conventions;
+using Microsoft.OData.Edm;
+using Xunit;
+using Xunit.Extensions;
+using System.Web.OData.Builder;
+
+namespace Microsoft.AspNet.OData.Test.GeneralPropertyAccess
+{
+    public class NavigationPropertyAccessTest
+    {
+        private readonly HttpConfiguration _configuration;
+        private HttpClient _client;
+
+        public NavigationPropertyAccessTest()
+        {
+            _configuration = new[]
+            {
+                typeof(MetadataController), typeof(NavCustomersController)
+            }.GetHttpConfiguration();
+
+            IEdmModel model = GetEdmModel();
+
+            // only convention routings
+            var routingConventions = ODataRoutingConventions.CreateDefault();
+            routingConventions.Insert(0, new NavPropertyRoutingConvention());
+            _configuration.MapODataServiceRoute("odata", "odata", model, new DefaultODataPathHandler(), routingConventions);
+
+            HttpServer server = new HttpServer(_configuration);
+            _client = new HttpClient(server);
+        }
+
+        [Fact]
+        public void QueryNavProperty()
+        {
+            string requestUri = "http://localhost/odata/NavCustomers(1)/Orders(2)";
+
+            string result = @"{
+  ""@odata.context"":""http://localhost/odata/$metadata#NavOrders/$entity"",""Id"":2
+}";
+
+            HttpResponseMessage response = _client.GetAsync(requestUri).Result;
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(result, response.Content.ReadAsStringAsync().Result);
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<NavCustomer>("NavCustomers");
+            builder.EntitySet<NavOrder>("NavOrders");
+            return builder.GetEdmModel();
+        }
+    }
+    public class NavCustomersController : ODataController
+    {
+        [HttpGet]
+        public IHttpActionResult GetOrders(int key, int relatedKey)
+        {
+            NavCustomer customer = new NavCustomer
+            {
+                Id = key,
+                Orders = Enumerable.Range(1, 5).Select(c => new NavOrder { Id = c }).ToList()
+            };
+
+            return Ok(customer.Orders.FirstOrDefault(o => o.Id == relatedKey));
+        }
+    }
+
+    public class NavCustomer
+    {
+        public int Id { get; set; }
+
+        public IList<NavOrder> Orders { get; set; }
+    }
+
+    public class NavOrder
+    {
+        public int Id { get; set; }
+    }
+}
