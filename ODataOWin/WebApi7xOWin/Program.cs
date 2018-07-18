da4f4f83d38867ab9c82b5d2c73a150901c99f16
Sam Xu <saxu@microsoft.com>
2018-07-18 15:51:29 -0700

    Add a OWin project to host OData service using Microsoft.AspNet.OData v7.0 and .NET Framework v4.6.2

---
+++ ODataOWin/WebApi7xOWin/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi7xOWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUri = "http://localhost:53234";
+
+            using (WebApp.Start<Startup>(url: baseUri))
+            {
+                Console.WriteLine("Service is running http://localhost:53234. Press any key to quit ....");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ODataOWin/WebApi7xOWin/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.OData.Edm;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApi7xOWin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration
+            {
+                IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always
+            };
+
+            config.MapODataServiceRoute("odata", "odata", GetEdmModel());
+
+
+            appBuilder.UseWebApi(config);
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var b = new ODataConventionModelBuilder();
+            return b.GetEdmModel();
+        }
+    }
+}
