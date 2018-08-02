8f4d0265233f7eecb30653782f0724d9f4b2f3db
Sam Xu <saxu@microsoft.com>
2018-08-02 11:36:49 -0700

    Add a simple function test for v6.1

---
+++ v6.x/SimpleFunctionTest/SimpleFunctionTest/Controllers/AppsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData;
+
+namespace SimpleFunctionTest.Controllers
+{
+    public class AppsController : ODataController
+    {
+        [HttpGet]
+        public IHttpActionResult Get([FromODataUri]int key)
+        {
+            return Ok(key);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Download([FromODataUri]int key, [FromODataUri]string p)
+        {
+            var r = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            var c = "Download (p=" + p + ") in Apps of key =" + key;
+            r.Content = new StringContent(c);
+            return r;
+        }
+    }
+}
---
+++ v6.x/SimpleFunctionTest/SimpleFunctionTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.OData.Edm;
+using SimpleFunctionTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace SimpleFunctionTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = GetClient();
+
+            var requestUri = @"http://localhost/odata/$metadata";
+            var response = client.GetAsync(requestUri).Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+
+            requestUri = @"http://localhost/odata/Apps(1)";
+            Console.WriteLine();
+
+            Console.WriteLine(requestUri);
+            response = client.GetAsync(requestUri).Result;
+
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+            //requestUri = @"http://localhost/odata/Apps(1)/Default.Download(p='txt')";
+              requestUri = @"http://localhost/odata/Apps(1)/Default.Download(p='Foo%2FBar%2Ftest.txt')";
+            Console.WriteLine();
+
+            Console.WriteLine(requestUri);
+            response = client.GetAsync(requestUri).Result;
+
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+        }
+
+        private static HttpClient GetClient()
+        {
+            var c = new HttpConfiguration();
+            c.MapODataServiceRoute("odata", "odata", GetEdmModel());
+            return new HttpClient(new HttpServer(c));
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var b = new ODataConventionModelBuilder();
+            var f = b.EntitySet<App>("Apps").EntityType.Function("Download").Returns<int>();
+            f.Parameter<string>("p");
+            return b.GetEdmModel();
+        }
+    }
+}
