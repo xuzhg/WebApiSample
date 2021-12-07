361a7a257158e0ace2712c35a8d4e75eff2fabbf
Sam Xu <saxu@microsoft.com>
2021-12-07 00:23:02 -0800

    Add a Linq Expression builde sample

---
+++ ODataQueryLinqExpressionBuilder/ODataQueryLinqExpressionBuilder/FilterBinder.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.UriParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODataQueryLinqExpressionBuilder
+{
+    internal class FilterBinder
+    {
+        private static readonly Dictionary<BinaryOperatorKind, ExpressionType> BinaryOperatorMapping = new Dictionary<BinaryOperatorKind, ExpressionType>
+        {
+            { BinaryOperatorKind.Add, ExpressionType.Add },
+            { BinaryOperatorKind.And, ExpressionType.AndAlso },
+            { BinaryOperatorKind.Divide, ExpressionType.Divide },
+            { BinaryOperatorKind.Equal, ExpressionType.Equal },
+            { BinaryOperatorKind.GreaterThan, ExpressionType.GreaterThan },
+            { BinaryOperatorKind.GreaterThanOrEqual, ExpressionType.GreaterThanOrEqual },
+            { BinaryOperatorKind.LessThan, ExpressionType.LessThan },
+            { BinaryOperatorKind.LessThanOrEqual, ExpressionType.LessThanOrEqual },
+            { BinaryOperatorKind.Modulo, ExpressionType.Modulo },
+            { BinaryOperatorKind.Multiply, ExpressionType.Multiply },
+            { BinaryOperatorKind.NotEqual, ExpressionType.NotEqual },
+            { BinaryOperatorKind.Or, ExpressionType.OrElse },
+            { BinaryOperatorKind.Subtract, ExpressionType.Subtract },
+        };
+
+        private IDictionary<string, ParameterExpression> _lambdaParameters = new Dictionary<string, ParameterExpression>();
+
+        //public IEdmModel Model { get; }
+
+        //public FilterBinder(IEdmModel model)
+        //{
+        //    Model = model;
+        //}
+
+        public virtual Expression Bind<T>(FilterClause filter)
+        {
+            ParameterExpression parameter = Expression.Parameter(typeof(T), filter.RangeVariable.Name);
+            _lambdaParameters[filter.RangeVariable.Name] = parameter;
+
+            Expression body = BindExpression(filter.Expression);
+
+            return Expression.Lambda(body, parameter);
+        }
+
+        private Expression BindExpression(QueryNode node)
+        {
+            if (node is CollectionNode collectionNode)
+            {
+                throw new NotSupportedException();
+            }
+            else if (node is SingleValueNode singleValueNode)
+            {
+                return BindSingleValueNode(singleValueNode);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        private Expression BindSingleValueNode(SingleValueNode node)
+        {
+            switch (node.Kind)
+            {
+                case QueryNodeKind.BinaryOperator:
+                    return BindBinaryOperatorNode(node as BinaryOperatorNode);
+
+                case QueryNodeKind.Constant:
+                    return BindConstantNode(node as ConstantNode);
+
+                case QueryNodeKind.Convert:
+                    return BindConvertNode(node as ConvertNode);
+
+                case QueryNodeKind.SingleComplexNode:
+                    return BindSingleComplexNode(node as SingleComplexNode);
+
+                case QueryNodeKind.ResourceRangeVariableReference:
+                    return BindRangeVariable((node as ResourceRangeVariableReferenceNode).RangeVariable);
+
+                case QueryNodeKind.NonResourceRangeVariableReference:
+                    return BindRangeVariable((node as NonResourceRangeVariableReferenceNode).RangeVariable);
+
+                case QueryNodeKind.SingleValuePropertyAccess:
+                    return BindPropertyAccessQueryNode(node as SingleValuePropertyAccessNode);
+
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        internal Expression CreatePropertyAccessExpression(Expression source, IEdmProperty property)
+        {
+            return Expression.Property(source, property.Name);
+        }
+
+        private Expression BindSingleComplexNode(SingleComplexNode singleComplexNode)
+        {
+            Expression source = BindExpression(singleComplexNode.Source);
+
+            return Expression.Property(source, singleComplexNode.Property.Name);
+        }
+
+        private Expression BindPropertyAccessQueryNode(SingleValuePropertyAccessNode propertyAccessNode)
+        {
+            Expression source = BindExpression(propertyAccessNode.Source);
+
+            return Expression.Property(source, propertyAccessNode.Property.Name);
+        }
+
+        private Expression BindConstantNode(ConstantNode constantNode)
+        {
+            if (constantNode.Value == null)
+            {
+                return Expression.Constant(null);
+            }
+
+            return Expression.Constant(constantNode.Value);
+        }
+
+        private Expression BindBinaryOperatorNode(BinaryOperatorNode binaryOperatorNode)
+        {
+            Expression left = BindExpression(binaryOperatorNode.Left);
+            Expression right = BindExpression(binaryOperatorNode.Right);
+
+            if (BinaryOperatorMapping.TryGetValue(binaryOperatorNode.OperatorKind, out ExpressionType binaryExpressionType))
+            {
+                return Expression.MakeBinary(binaryExpressionType, left, right);
+            }
+
+            throw new NotSupportedException();
+        }
+
+        private Expression BindConvertNode(ConvertNode convertNode)
+        {
+            return null;
+        }
+
+        private Expression BindRangeVariable(RangeVariable rangeVariable)
+        {
+            return _lambdaParameters[rangeVariable.Name];
+        }
+    }
+}
---
+++ ODataQueryLinqExpressionBuilder/ODataQueryLinqExpressionBuilder/Program.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OData.UriParser;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using ExpressionTreeToString;
+using System.Reflection;
+using System.Linq;
+
+namespace ODataQueryLinqExpressionBuilder
+{
+    internal class Program
+    {
+        static IList<Customer> _customers = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "Peter", Age= 36, Location = new Address { City = "Redmond", Street = "156TH AVE"} },
+            new Customer { Id = 2, Name = "Sam", Age= 34, Location = new Address { City = "Shanghai", Street = "Zixin Rd"}},
+            new Customer { Id = 3, Name = "John", Age= 12, Location = new Address { City = "Beijing", Street = "Yunan Rd"}},
+            new Customer { Id = 4, Name = "Kerry", Age= 24, Location = new Address { City = "Seattle", Street = "145TH NE ST"}},
+            new Customer { Id = 5, Name = "Alex", Age= 78, Location = new Address { City = "Sammamish", Street = "225TH AVE"}}
+        };
+
+        static void Main(string[] args)
+        {
+            // Build the model
+            IEdmModel model = GetEdmModel();
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Sample Data:");
+                Print(ConsoleColor.Green, _customers, true);
+
+                Console.WriteLine("\nPlease input $filter (For example: Location/City eq 'Shanghai' or Q/q for quit):");
+                string cmd = Console.ReadLine();
+                if (cmd == "q" || cmd == "Q")
+                {
+                    break;
+                }
+
+                // Query parser
+                // "Location/City eq 'Shanghai'"
+                // Age lt 30 
+                FilterClause filter;
+                try
+                {
+                    filter = ParseFilter(model, cmd);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n[Error]: {ex.Message},\nPress any key to try more...");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                // Linq Expression
+                FilterBinder filterBinder = new FilterBinder(/*model*/);
+                Expression filterExp = filterBinder.Bind<Customer>(filter);
+
+                string filterStr = filterExp.ToString("C#");
+                Console.WriteLine($"\n\t==> {filterStr}\n");
+
+                IEnumerable<Customer> filtered = Invoke(_customers, filterExp);
+
+                Console.WriteLine("Filter Result:");
+                Print(ConsoleColor.Blue, filtered);
+
+                Console.WriteLine("Press any key to try more");
+                Console.ReadKey();
+            }
+
+            // Invoke
+            Console.WriteLine("Nice Job!");
+        }
+
+
+        private static void Print(ConsoleColor color, IEnumerable<Customer> customers, bool head = false)
+        {
+            ConsoleColor backColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            Console.WriteLine();
+            if (head)
+            {
+                Console.WriteLine($"  | ID |      Name|  Age  |     City     |     Street    |");
+                Console.WriteLine($"  --------------------------------------------------------");
+            }
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"  |  {customer.Id} |{customer.Name,10}|  {customer.Age}  |{customer.Location.City,15}|{customer.Location.Street,15}|");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = backColor;
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            //var model = new EdmModel();
+            //var address = new EdmComplexType("NS", "Address");
+            //address.AddStructuralProperty("City", EdmPrimitiveTypeKind.String);
+            //address.AddStructuralProperty("Street", EdmPrimitiveTypeKind.String);
+            //model.AddElement(address);
+
+            //var customer = new EdmEntityType("NS", "Customer");
+            //customer.AddKeys(customer.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
+            //customer.AddStructuralProperty("Name", EdmPrimitiveTypeKind.String);
+            //customer.AddStructuralProperty("Age", EdmPrimitiveTypeKind.Int32);
+            //customer.AddStructuralProperty("Location", new EdmComplexTypeReference(address, true));
+
+            //model.AddElement(customer);
+
+            //var container = new EdmEntityContainer("NS", "Default");
+            //container.AddEntitySet("Customers", customer);
+            //model.AddElement(container);
+            //return model;
+
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Customer>("Customers");
+            return builder.GetEdmModel();
+        }
+
+        private static FilterClause ParseFilter(IEdmModel model, string filter)
+        {
+            var customers = model.EntityContainer.FindEntitySet("Customers");
+            var customer = customers.EntityType();
+
+            var parser = new ODataQueryOptionParser(model, customer, customers, new Dictionary<string, string>
+            {
+                {"$filter", filter},
+            });
+
+            return parser.ParseFilter();
+        }
+
+        private static IEnumerable<T> Invoke<T>(IEnumerable<T> source, Expression expression)
+        {
+            MethodInfo whereMethod = GetEnumerableWhere();
+
+            whereMethod = whereMethod.MakeGenericMethod(typeof(T));
+
+            Func<T, bool> function = (expression as LambdaExpression).Compile() as Func<T, bool>;
+            return whereMethod.Invoke(null, new object[] { source, function }) as IEnumerable<T>;
+        }
+
+        private static MethodInfo GetEnumerableWhere()
+        {
+            // public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+            // public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, int, bool> predicate)
+            var whereMethods = typeof(Enumerable).GetMethods().Where(x => x.Name == "Where");
+
+            foreach (var whereMethod in whereMethods)
+            {
+                ParameterInfo[] parameters = whereMethod.GetParameters();
+                if (parameters[1].ParameterType.GetGenericArguments().Length == 2)
+                {
+                    return whereMethod;
+                }
+            }
+
+            throw new NotSupportedException();
+        }
+    }
+
+    public class Address
+    {
+        public string City { get; set; }
+
+        public string Street { get; set; }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Address Location { get; set; }
+    }
+}
