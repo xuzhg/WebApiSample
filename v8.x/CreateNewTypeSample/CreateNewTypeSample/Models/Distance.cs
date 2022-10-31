3a267d28e9e48ff9a94caab972158aec6281421e
Sam Xu <saxu@microsoft.com>
2022-10-31 12:39:56 -0700

    Make the post working

--- v8.x/CreateNewTypeSample/CreateNewTypeSample/Models/Distance.cs
+++ v8.x/CreateNewTypeSample/CreateNewTypeSample/Models/Distance.cs
@@ -10,6 +10,29 @@
 
     public class Distance
     {
+        public Distance()
+        {
+
+        }
+
+        public Distance(string value)
+        {
+            int index = 0;
+            for (; index < value.Length; index++)
+            {
+                if (!char.IsDigit(value[index]))
+                {
+                    break;
+                }
+            }
+
+            string left = value.Substring(0, index);
+            string right = value.Substring(index);
+
+            Value = double.Parse(left);
+            Unit = Enum.Parse<DistanceUnit>(right.ToUpper());
+        }
+
         public double Value { get; set; }
 
         public DistanceUnit Unit { get; set; }
