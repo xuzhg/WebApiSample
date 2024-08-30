383085ff91711b68bff8b542a5a69855ed0a9561
Sihanook Fernando <sifernan@microsoft.com>
2024-08-30 16:10:03 -0700

    Update CommsEntityController.cs

--- v8.x/ODataDyncRouteSample/ODataDyncRouteSample/Controllers/CommsEntityController.cs
+++ v8.x/ODataDyncRouteSample/ODataDyncRouteSample/Controllers/CommsEntityController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Query.Wrapper;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 
 using ODataDyncRouteSample.Models;
@@ -9,10 +12,9 @@ namespace ODataDyncRouteSample.Controllers
     public class CommsEntityController<T> : ODataController
         where T : CommsEntity, new()
     {
-        [EnableQuery]
-        public Task<ActionResult<IQueryable<T>>> GetAsync(ODataQueryOptions<T> queryOptions)
+        public Task<ActionResult<ODataResponse<T>>> GetAsync(ODataQueryOptions<T> queryOptions)
         {
-            List<CommsEntity> result = new List<CommsEntity>();
+            List<AzureUpdate> result = new List<AzureUpdate>();
             result.Add(new AzureUpdate
             {
                 Id = "1",
@@ -20,8 +22,51 @@ namespace ODataDyncRouteSample.Controllers
                 Description = "Azure Update 1 Description",
             });
 
-            IQueryable<T> queryable = result.AsQueryable().Cast<T>();
-            ActionResult<IQueryable<T>> actionResult = Ok(queryable);
+            result.Add(new AzureUpdate
+            {
+                Id = "2",
+                Title = "Azure Update 2",
+                Description = "Azure Update 2 Description",
+            });
+
+            IQueryable<AzureUpdate> resultsAsQueryable = result.AsQueryable();
+            IQueryable filteredResults = queryOptions.ApplyTo(resultsAsQueryable);
+
+            // There is a known issue where converting selected data back to a list is not easily supported.
+            // See the GitHub issue here: https://github.com/OData/AspNetCoreOData/issues/865
+            // This is a workaround to handle that scenario until it is better supported officially.
+            if (typeof(ISelectExpandWrapper).IsAssignableFrom(filteredResults.ElementType))
+            {
+                List<AzureUpdate> reprojectedResults = new List<AzureUpdate>();
+                foreach (object filteredItem in filteredResults)
+                {
+                    ISelectExpandWrapper wrappedFilteredItem = filteredItem as ISelectExpandWrapper;
+                    IDictionary<string, object> selectedPropertyValues = wrappedFilteredItem.ToDictionary();
+
+                    AzureUpdate recreatedItem = new AzureUpdate();
+                    foreach (PropertyInfo entityTypeProperty in typeof(T).GetProperties())
+                    {
+                        if (selectedPropertyValues.ContainsKey(entityTypeProperty.Name))
+                        {
+                            entityTypeProperty.SetValue(recreatedItem, selectedPropertyValues[entityTypeProperty.Name]);
+                        }
+                    }
+
+                    reprojectedResults.Add(recreatedItem);
+                }
+
+                result = reprojectedResults;
+            }
+            else
+            {
+                result = filteredResults.Cast<AzureUpdate>().ToList();
+            }
+
+            ODataResponse<T> response = new ODataResponse<T>();
+            response.Value = new ODataValue<T>();
+            response.Value.Content = (ICollection<T>)result;
+
+            ActionResult<ODataResponse<T>> actionResult = Ok(response);
 
             return Task.FromResult(actionResult);
         }
