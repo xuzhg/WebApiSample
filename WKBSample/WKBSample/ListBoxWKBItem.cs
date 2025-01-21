02d5c4f5fdd88942f2f1d942c8a13fe11c132e61
Sam Xu <saxu@microsoft.com>
2025-01-21 09:45:28 -0800

    Update listBoxWKBItem into its own namespace

--- WKBSample/WKBSample/ListBoxWKBItem.cs
+++ WKBSample/WKBSample/ListBoxWKBItem.cs
@@ -3,30 +3,27 @@ using WKBSample.WBK;
 
 namespace WKBSample;
 
-public partial class Form1
+internal class ListBoxWKBItem
 {
-    internal class ListBoxWKBItem
+    public int Indent { get; set; }
+    public IWKBObject WkbObject { get; set; }
+    public ContextMenuStrip ContextMenuStrip { get; set; }
+
+    public override string ToString()
     {
-        public int Indent { get; set; }
-        public IWKBObject WkbObject { get; set; }
-        public ContextMenuStrip ContextMenuStrip { get; set; }
-
-        public override string ToString()
+        StringBuilder sb = new StringBuilder(Indent * 4 + 2);
+        for (int i = 0; i < Indent; i++)
         {
-            StringBuilder sb = new StringBuilder(Indent * 4 + 2);
-            for (int i = 0; i < Indent; i++)
+            if (i != Indent - 1)
             {
-                if (i != Indent - 1)
-                {
-                    sb.Append("    ");
-                }
-                else
-                {
-                    sb.Append("    |-");
-                }
+                sb.Append("    ");
             }
-
-            return $"{sb.ToString()} {WkbObject.ToString()}";
+            else
+            {
+                sb.Append("    |-");
+            }
         }
+
+        return $"{sb.ToString()} {WkbObject.ToString()}";
     }
 }
