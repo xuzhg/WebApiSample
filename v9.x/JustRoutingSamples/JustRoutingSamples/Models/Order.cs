e72a04f92d466f692ae127e196f07db9eca9c839
Sam Xu <saxu@microsoft.com>
2025-10-14 10:25:32 -0700

    Sample for issue: https://github.com/OData/AspNetCoreOData/issues/1532

---
+++ v9.x/JustRoutingSamples/JustRoutingSamples/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JustRoutingSamples.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string ProductName { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ v9.x/JustRoutingSamples/JustRoutingSamples/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace JustRoutingSamples.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public IList<Order> Orders { get; set; }
+    }
+}
